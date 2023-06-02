b11133db071fcbb779c28b0a0dea8b529634c4f2
AmielRe <amieli1999@gmail.com>
2023-06-02 23:09:36 +0300

    Create backend skeleton.

---
+++ Backend/FileUploadService/src/test/kotlin/com/amiel/fileuploadservice/FileUploadServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.amiel.fileuploadservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FileUploadServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
---
+++ Backend/UserManagementService/src/test/java/com/amiel/usermanagementservice/UserManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amiel.usermanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
