3015ede261b638a29e07b336baa2f104ad4a330f
AmielRe <amieli1999@gmail.com>
2023-06-02 23:10:00 +0300

    Create frontend skeleton.

---
+++ Frontend/securescan/src/App.js
@@ -0,0 +1,20 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+
+function App() {
+  return (
+    <>
+      <nav className="navbar bg-dark">
+        <div className="container-fluid">
+          <span className="appName">React User Authentication</span>
+        </div>
+      </nav>
+      <LoginButton />
+      <LogoutButton />
+    </>
+  );
+}
+
+export default App;
