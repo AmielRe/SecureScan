188d282877bc1b2a7bfd8350410727acb7bf5a56
AmielRe <amieli1999@gmail.com>
2023-06-10 19:10:34 +0300

    Implement controller api methods.
    Move controller package and remove the scan base packages.
    Implement "signin", "signup" and "signout" api methods.

--- Backend/UserManagementService/src/main/java/com/amiel/usermanagementservice/UserManagementServiceApplication.java
+++ Backend/UserManagementService/src/main/java/com/amiel/usermanagementservice/UserManagementServiceApplication.java
@@ -3,7 +3,7 @@ package com.amiel.usermanagementservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "controllers")
+@SpringBootApplication
 public class UserManagementServiceApplication {
 
     public static void main(String[] args) {
